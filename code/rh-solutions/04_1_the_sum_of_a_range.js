368b8cd35f46ad9ffd63c91019f8a9c8acb41ce4
reneehu <reneehu217@gmail.com>
2017-02-28 17:07:54 -0500

    range and summ

--- code/rh-solutions/04_1_the_sum_of_a_range.js
+++ code/rh-solutions/04_1_the_sum_of_a_range.js
@@ -1,1 +1,25 @@
--n 
+
+function range(start, end, step){
+	var ary = [];
+	step = (step !== undefined) ? step : 1;
+	if(step>0){
+		for(var i = start; i <= end; i+=step){
+			ary.push(i);
+		}
+	}else{
+		for(var i = start; i >= end; i+=step){
+			ary.push(i);
+		}
+	}
+	return ary;
+}
+range(1,10);
+
+function sum(array){
+	var sumUp = 0;
+	for(i=0; i < array.length; i++){
+		sumUp += array[i];
+	}
+	return sumUp;
+}
+sum(range(1,10));
