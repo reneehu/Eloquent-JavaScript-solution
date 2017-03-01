ff10fc34f028fcb5b6009889e8e5ebafac39d0ab
reneehu <reneehu217@gmail.com>
2017-03-01 12:49:18 -0500

    arrayyy

--- code/rh-solutions/04_2_reversing_an_array.js
+++ code/rh-solutions/04_2_reversing_an_array.js
@@ -1,1 +1,21 @@
--n 
+function reverseArray(array) {
+	var reversed = [];
+	for(i=(array.length-1); i>=0; i--){
+		reversed.push(array[i]);
+	}
+	return reversed;
+}
+
+reverseArray([1,2,3]);
+
+function reverseArrayInPlace(array) {
+	for (var i = 0; i < Math.floor(array.length / 2); i++) {
+		var old = array[i];
+		array[i] = array[array.length - 1 - i];
+		array[array.length - 1 - i] = old;
+	}
+	return array;
+}
+var arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+arrayValue
