947ae71f1d8a3ed5a22a3272d89f69730834e5e3
reneehu <reneehu217@gmail.com>
2017-03-03 18:04:28 -0500

    deeeep compare/reduce/concat

--- code/rh-solutions/05_1_flattening.js
+++ code/rh-solutions/05_1_flattening.js
@@ -1,1 +1,11 @@
--n 
+function flatten(arrays) {
+	return arrays.reduce(
+		function(a, b) {
+			console.log('a is' + a + ' ' + 'b is' + b);
+			return a.concat(b);
+		},[]
+		);
+}
+
+var arrays = [[1, 2, 3], [4, 5], [6]];
+flatten(arrays);
