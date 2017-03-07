34baf16c7fcb5c6c3aea1118eeae9ec94dd8d584
reneehu <reneehu217@gmail.com>
2017-03-07 18:35:21 -0500

    higher order functions

--- code/rh-solutions/05_1_flattening.js
+++ code/rh-solutions/05_1_flattening.js
@@ -9,3 +9,29 @@ function flatten(arrays) {
 
 var arrays = [[1, 2, 3], [4, 5], [6]];
 flatten(arrays);
+
+// function flat(ary) {
+// 	return ary.reduce( function(a,b){
+// 		return a.concat(b);
+// 	} ,[]);
+// }
+
+
+// var arrays = [[1, 2, 3], [4, 5], [6]];
+// flat(arrays);
+
+
+function map(array, transform) {
+  var mapped = [];
+  for (var i = 0; i < array.length; i++)
+    mapped.push(transform(array[i]));
+  return mapped;
+}
+
+var overNinety = ancestry.filter(function(person) {
+  return person.died - person.born > 90;
+});
+
+console.log(map(overNinety, function(person) {
+  return person.name;
+}));
--- code/rh-solutions/05_2_mother-child_age_difference.js
+++ code/rh-solutions/05_2_mother-child_age_difference.js
@@ -1,1 +1,18 @@
--n 
+function average(array) {
+	function plus(a, b) { return a + b;}
+	return array.reduce(plus) / array.length;
+}
+
+var byName = {};
+ancestry.forEach(function(person) {
+	byName[person.name] = person;
+});
+
+
+var differences = ancestry.filter(function(person) {
+	return byName[person.mother] != null;
+}).map(function(person) {
+   return person.born - byName[person.mother].born;
+});
+
+average(differences);
